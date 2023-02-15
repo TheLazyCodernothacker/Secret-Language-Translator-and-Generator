20c3414d6a3f190d59bad0f03fad1f8daa785dd7
TheLazyCodernothacker <120674735+TheLazyCodernothacker@users.noreply.github.com>
2023-02-15 21:56:58 +0000

    Pending changes exported from your codespace

---
+++ main.js
@@ -0,0 +1,168 @@
+function change(val){
+    var newChar = '';
+    switch (val) {
+        case 'a':
+            var newChar = '1'
+            break;
+        case 'A':
+            var newChar = "1"
+            break;
+        case 'b':
+            var newChar = "7"
+            break
+        case 'B':
+            var newChar = "7"
+            break;
+        case 'c':
+            var newChar = "5"
+            break
+        case 'C':
+            var newChar = "5"
+            break;  
+        case 'd':
+            var newChar = "2"
+            break
+        case 'D':
+            var newChar = "2"
+            break;  
+        case 'e':
+            var newChar = "!"
+            break
+        case 'E':
+            var newChar = "!"
+            break;    
+        case 'f':
+            var newChar = "0"
+            break
+        case 'F':
+            var newChar = "0"
+            break;  
+        case 'g':
+            var newChar = "4"
+            break
+        case 'G':
+            var newChar = "4"
+            break;  
+        case 'h':
+            var newChar = "#"
+            break
+        case 'H':
+            var newChar = "#"
+            break;
+        case 'i':
+            var newChar = "3"
+            break
+        case 'I':
+            var newChar = "3"
+            break;     
+        case 'j':
+            var newChar = "$"
+            break
+        case 'J':
+            var newChar = "$"
+            break;  
+        case 'k':
+            var newChar = "("
+            break
+        case 'K':
+            var newChar = "("
+            break;   
+        case 'l':
+            var newChar = "8"
+            break
+        case 'L':
+            var newChar = "8"
+            break; 
+        case 'm':
+            var newChar = "-"
+            break
+        case 'M':
+            var newChar = "-"
+            break; 
+        case 'n':
+            var newChar = "9"
+            break
+        case 'N':
+            var newChar = "9"
+            break;
+        case 'o':
+            var newChar = "_"
+            break
+        case 'O':
+            var newChar = "_"
+            break;
+        case 'p':
+            var newChar = "%"
+            break
+        case 'P':
+            var newChar = "%"
+            break;
+        case 'q':
+            var newChar = "@"
+            break
+        case 'Q':
+            var newChar = "@"
+            break;
+        case 'r':
+            var newChar = "6"
+            break
+        case 'R':
+            var newChar = "6"
+            break;
+        case 's':
+            var newChar = ")"
+            break
+        case 'S':
+            var newChar = ")"
+            break;
+        case 't':
+            var newChar = "["
+            break
+        case 'T':
+            var newChar = "["
+            break;
+        case 'u':
+            var newChar = "?"
+            break
+        case 'U':
+            var newChar = "?"
+            break;
+        case 'v':
+            var newChar = "+"
+            break
+        case 'V':
+            var newChar = "+"
+            break;
+        case 'w':
+            var newChar = "{"
+            break
+        case 'W':
+            var newChar = "{"
+            break;
+        case 'x':
+            var newChar = "~"
+            break
+        case 'X':
+            var newChar = "~"
+            break;
+        case 'y':
+            var newChar = "]"
+            break
+        case 'Y':
+            var newChar = "]"
+            break;
+        case 'z':
+            var newChar = "`"
+            break
+        case 'Z':
+            var newChar = "`"
+            break;
+        case ' ':
+            var newChar = "|"
+            break;
+        case ' ':
+            var newChar = "|"
+            break;
+    }
+    return newChar
+}
