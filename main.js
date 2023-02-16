19870d2c2a3f53db17bc11bba2d685a62dd14fa1
TheLazyCodernothacker <120674735+TheLazyCodernothacker@users.noreply.github.com>
2023-02-16 08:23:29 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,168 +1,584 @@
+/* 
 function change(val){
     var newChar = '';
     switch (val) {
-        case 'a':
-            var newChar = '1'
-            break;
-        case 'A':
-            var newChar = "1"
-            break;
-        case 'b':
-            var newChar = "7"
-            break
-        case 'B':
-            var newChar = "7"
-            break;
-        case 'c':
-            var newChar = "5"
-            break
-        case 'C':
-            var newChar = "5"
-            break;  
-        case 'd':
-            var newChar = "2"
-            break
-        case 'D':
-            var newChar = "2"
-            break;  
-        case 'e':
-            var newChar = "!"
-            break
-        case 'E':
-            var newChar = "!"
-            break;    
-        case 'f':
-            var newChar = "0"
-            break
-        case 'F':
-            var newChar = "0"
-            break;  
-        case 'g':
-            var newChar = "4"
-            break
-        case 'G':
-            var newChar = "4"
-            break;  
-        case 'h':
-            var newChar = "#"
-            break
-        case 'H':
-            var newChar = "#"
-            break;
-        case 'i':
-            var newChar = "3"
-            break
-        case 'I':
-            var newChar = "3"
-            break;     
-        case 'j':
-            var newChar = "$"
-            break
-        case 'J':
-            var newChar = "$"
-            break;  
-        case 'k':
-            var newChar = "("
-            break
-        case 'K':
-            var newChar = "("
-            break;   
-        case 'l':
-            var newChar = "8"
-            break
-        case 'L':
-            var newChar = "8"
-            break; 
-        case 'm':
-            var newChar = "-"
-            break
-        case 'M':
-            var newChar = "-"
-            break; 
-        case 'n':
-            var newChar = "9"
-            break
-        case 'N':
-            var newChar = "9"
-            break;
-        case 'o':
-            var newChar = "_"
-            break
-        case 'O':
-            var newChar = "_"
-            break;
-        case 'p':
-            var newChar = "%"
-            break
-        case 'P':
-            var newChar = "%"
-            break;
-        case 'q':
-            var newChar = "@"
-            break
-        case 'Q':
-            var newChar = "@"
-            break;
-        case 'r':
-            var newChar = "6"
-            break
-        case 'R':
-            var newChar = "6"
-            break;
-        case 's':
-            var newChar = ")"
-            break
-        case 'S':
-            var newChar = ")"
-            break;
-        case 't':
-            var newChar = "["
-            break
-        case 'T':
-            var newChar = "["
-            break;
-        case 'u':
-            var newChar = "?"
-            break
-        case 'U':
-            var newChar = "?"
-            break;
-        case 'v':
-            var newChar = "+"
-            break
-        case 'V':
-            var newChar = "+"
-            break;
-        case 'w':
-            var newChar = "{"
-            break
-        case 'W':
-            var newChar = "{"
-            break;
-        case 'x':
-            var newChar = "~"
-            break
-        case 'X':
-            var newChar = "~"
-            break;
-        case 'y':
-            var newChar = "]"
-            break
-        case 'Y':
-            var newChar = "]"
-            break;
-        case 'z':
-            var newChar = "`"
-            break
-        case 'Z':
-            var newChar = "`"
-            break;
-        case ' ':
-            var newChar = "|"
-            break;
-        case ' ':
-            var newChar = "|"
-            break;
+        case 'a':var newChar = 'w';break;
+
+        case 'b':var newChar = 'Y';break;
+        
+        case 'c':var newChar = 'l';break;
+        
+        case 'd':var newChar = 'b';break;
+        
+        case 'e':var newChar = 'o';break;
+        case 'f':var newChar = '4';break;
+        
+        case 'g':var newChar = '=';break;
+        
+        case 'h':var newChar = 'Q';break;
+        
+        case 'i':var newChar = '"';break;
+        
+        case 'j':var newChar = '.';break;
+        
+        case 'k':var newChar = '(';break;
+        
+        case 'l':var newChar = 'z';break;
+        
+        case 'm':var newChar = 'R';break;
+        
+        case 'n':var newChar = 'V';break;
+        
+        case 'o':var newChar = ',';break;
+        
+        case 'p':var newChar = '1';break;
+        
+        case 'q':var newChar = 'T';break;
+        
+        case 'r':var newChar = 'S';break;
+        
+        case 's':var newChar = 'G';break;
+        
+        case 't':var newChar = 'A';break;
+        case 'u':var newChar = "'";break;
+
+        case 'v':var newChar = '*';break;
+        
+        case 'w':var newChar = '0';break;
+        
+        case 'x':var newChar = 'd';break;
+        
+        case 'y':var newChar = 'Z';break;
+        
+        case 'z':var newChar = 'I';break;
+        
+        case '%':var newChar = 'n';break;
+        
+        case '^':var newChar = 'D';break;
+        
+        case '&':var newChar = 'm';break;
+        
+        case '*':var newChar = 'J';break;
+        
+        case '(':var newChar = 'W';break;
+        
+        case ')':var newChar = 'X';break;
+        
+        case ',':var newChar = '3';break;
+        
+        case '.':var newChar = '&';break;
+        
+        case ':':var newChar = 'E';break;
+        
+        case "'":var newChar = '6';break;
+        
+        case '"':var newChar = 'O';break;
+        
+        case '+':var newChar = 'y';break;
+        
+        case '=':var newChar = 'u';break;
+        
+        case '-':var newChar = 'C';break;
+        
+        case '_':var newChar = '8';break;
+        
+        case '?':var newChar = '_';break;
+        case '1':var newChar = 'f';break;
+
+        case '2':var newChar = '-';break;
+        
+        case '3':var newChar = 'j';break;
+        
+        case '4':var newChar = '2';break;
+        
+        case '5':var newChar = '9';break;
+        
+        case '6':var newChar = '+';break;
+        
+        case '7':var newChar = '5';break;
+        
+        case '8':var newChar = 'K';break;
+        
+        case '9':var newChar = 'v';break;
+        
+        case '0':var newChar = 'H';break;
+        
+        case 'A':var newChar = '?';break;
+        
+        case 'B':var newChar = 'q';break;
+        
+        case 'C':var newChar = 'U';break;
+        
+        case 'D':var newChar = 'k';break;
+        
+        case 'E':var newChar = '^';break;
+        
+        case 'F':var newChar = 'x';break;
+        
+        case 'G':var newChar = 's';break;
+        
+        case 'H':var newChar = 'M';break;
+        
+        case 'I':var newChar = ')';break;
+        
+        case 'J':var newChar = 'c';break;
+        
+        case 'K':var newChar = 'h';break;
+        
+        case 'L':var newChar = 'B';break;
+        
+        case 'M':var newChar = 't';break;
+        case 'N':var newChar = 'a';break;
+
+        case 'O':var newChar = 'N';break;
+        
+        case 'P':var newChar = 'P';break;
+        
+        case 'Q':var newChar = 'i';break;
+        
+        case 'R':var newChar = 'L';break;
+        
+        case 'S':var newChar = '%';break;
+        
+        case 'T':var newChar = '7';break;
+        
+        case 'U':var newChar = 'F';break;
+        
+        case 'V':var newChar = 'p';break;
+        
+        case 'W':var newChar = 'e';break;
+        
+        case 'X':var newChar = ':';break;
+        
+        case 'Y':var newChar = 'r';break;
+        
+        case 'Z':var newChar = 'g';break;
+        case ' ':var newChar = '|';break;
+    }
+    return newChar
+}
+function changeBack(val){
+    var newChar = '';
+    switch (val) {
+        case 'w':var newChar = 'a';break;
+
+        case 'Y':var newChar = 'b';break;
+        
+        case 'l':var newChar = 'c';break;
+        
+        case 'b':var newChar = 'd';break;
+        case 'o':var newChar = 'e';break;
+        
+        case '4':var newChar = 'f';break;
+        
+        case '=':var newChar = 'g';break;
+        
+        case 'Q':var newChar = 'h';break;
+        
+        case '"':var newChar = 'i';break;
+        
+        case '.':var newChar = 'j';break;
+        
+        case '(':var newChar = 'k';break;
+        
+        case 'z':var newChar = 'l';break;
+        
+        case 'R':var newChar = 'm';break;
+        
+        case 'V':var newChar = 'n';break;
+        
+        case ',':var newChar = 'o';break;
+        
+        case '1':var newChar = 'p';break;
+        
+        case 'T':var newChar = 'q';break;
+        
+        case 'S':var newChar = 'r';break;
+        
+        case 'G':var newChar = 's';break;
+        
+        case 'A':var newChar = 't';break;
+        case "'":var newChar = "u";break;
+
+        case '*':var newChar = 'v';break;
+        
+        case '0':var newChar = 'w';break;
+        
+        case 'd':var newChar = 'x';break;
+        
+        case 'Z':var newChar = 'y';break;
+        
+        case 'I':var newChar = 'z';break;
+        
+        case 'n':var newChar = '%';break;
+        
+        case 'D':var newChar = '^';break;
+        
+        case 'm':var newChar = '&';break;
+        
+        case 'J':var newChar = '*';break;
+        
+        case 'W':var newChar = '(';break;
+        
+        case 'X':var newChar = ')';break;
+        
+        case '3':var newChar = ',';break;
+        
+        case '&':var newChar = '.';break;
+        
+        case 'E':var newChar = ':';break;
+        
+        case "6":var newChar = "'";break;
+        
+        case 'O':var newChar = '"';break;
+        
+        case 'y':var newChar = '+';break;
+        
+        case 'u':var newChar = '=';break;
+        
+        case 'C':var newChar = '-';break;
+        
+        case '8':var newChar = '_';break;
+        
+        case '_':var newChar = '?';break;
+        case 'f':var newChar = '1';break;
+
+        case '-':var newChar = '2';break;
+        
+        case 'j':var newChar = '3';break;
+        
+        case '2':var newChar = '4';break;
+        
+        case '9':var newChar = '5';break;
+        
+        case '+':var newChar = '6';break;
+        
+        case '5':var newChar = '7';break;
+        
+        case 'K':var newChar = '8';break;
+        
+        case 'v':var newChar = '9';break;
+        
+        case 'H':var newChar = '0';break;
+        
+        case '?':var newChar = 'A' ;break;
+        
+        case 'q':var newChar = 'B';break;
+        
+        case 'U':var newChar = 'C';break;
+        
+        case 'k':var newChar = 'D';break;
+        
+        case '^':var newChar = 'E';break;
+        
+        case 'x':var newChar = 'F';break;
+        
+        case 's':var newChar = 'G';break;
+        
+        case 'M':var newChar = 'H';break;
+        
+        case ')':var newChar = 'I';break;
+        
+        case 'c':var newChar = 'J';break;
+        
+        case 'h':var newChar = 'K';break;
+        
+        case 'B':var newChar = 'L';break;
+        
+        case 't':var newChar = 'M';break;
+        case 'a':var newChar = 'N';break;
+
+        case 'N':var newChar = 'O';break;
+        
+        case 'P':var newChar = 'P';break;
+        
+        case 'i':var newChar = 'Q';break;
+        
+        case 'L':var newChar = 'R';break;
+        
+        case '%':var newChar = 'S';break;
+        
+        case '7':var newChar = 'T';break;
+        
+        case 'F':var newChar = 'U';break;
+        
+        case 'p':var newChar = 'V';break;
+        
+        case 'e':var newChar = 'W';break;
+        
+        case ':':var newChar = 'X';break;
+        
+        case 'r':var newChar = 'Y';break;
+        
+        case 'g':var newChar = 'Z';break;
+        case '|':var newChar = ' ';break;
+    }
+    return newChar
+}
+*/
+function change(val){
+    var newChar = '';
+    switch (val) {
+        case ' ':var newChar = '|';break;
+        case 'q':var newChar = '2';break;
+
+case 'w':var newChar = 'U';break;
+
+case 'e':var newChar = '$';break;
+
+case 'r':var newChar = '*';break;
+
+case 't':var newChar = '~';break;
+
+case 'y':var newChar = '8';break;
+
+case 'u':var newChar = ':';break;
+
+case 'i':var newChar = 'J';break;
+
+case 'o':var newChar = '.';break;
+
+case 'p':var newChar = 'C';break;
+
+case '[':var newChar = "'";break;
+
+case ']':var newChar = 'v';break;
+
+case 'q':var newChar = 'O';break;
+
+case 'W':var newChar = ']';break;
+
+case 'E':var newChar = 'p';break;
+
+case 'R':var newChar = 'n';break;
+
+case 'T':var newChar = 'a';break;
+
+case 'Y':var newChar = 'P';break;
+case 'U':var newChar = '1';break;
+
+case 'I':var newChar = 'B';break;
+
+case 'O':var newChar = 'z';break;
+
+case 'P':var newChar = 'r';break;
+
+case '{':var newChar = 'W';break;
+
+case '}':var newChar = ',';break;
+
+case 'a':var newChar = '@';break;
+
+case 's':var newChar = 'N';break;
+
+case 'd':var newChar = 'F';break;
+
+case 'f':var newChar = '0';break;
+
+case 'g':var newChar = '}';break;
+
+case 'h':var newChar = '=';break;
+
+case 'j':var newChar = '"';break;
+
+case 'k':var newChar = 'u';break;
+
+case 'l':var newChar = '^';break;
+
+case ';':var newChar = '<';break;
+
+case "'":var newChar = 'X';break;
+
+case 'A':var newChar = '/';break;
+
+case 'S':var newChar = 'T';break;
+
+case 'D':var newChar = 'm';break;
+
+case 'F':var newChar = 's';break;
+
+case 'G':var newChar = 'k';break;
+
+case 'H':var newChar = 'K';break;
+
+case 'J':var newChar = 'M';break;
+
+case 'K':var newChar = 'g';break;
+
+case 'L':var newChar = 'b';break;
+
+case ':':var newChar = '#';break;
+
+case '"':var newChar = 'D';break;
+
+case 'z':var newChar = 'Y';break;
+
+case 'x':var newChar = 'H';break;
+case '"':var newChar = 'D';break;
+case 'z':var newChar = 'Y';break;
+case 'x':var newChar = 'H';break;
+case 'c':var newChar = '>';break;
+case 'v':var newChar = 'q';break;
+case 'b':var newChar = 'w';break;
+case 'n':var newChar = 'E';break;
+case 'm':var newChar = '9';break;
+case ',':var newChar = ';';break;
+case '.':var newChar = ')';break;
+case '/':var newChar = 'f';break;
+case 'Z':var newChar = '+';break;
+case 'X':var newChar = '6';break;
+case 'C':var newChar = 'G';break;
+case 'V':var newChar = 'I';break;
+case 'B':var newChar = 'h';break;
+case 'N':var newChar = '7';break;
+case 'M':var newChar = 'x';break;
+case '<':var newChar = 'V';break;
+case '>':var newChar = 'd';break;
+case '?':var newChar = 'i';break;
+case '1':var newChar = '{';break;
+case '2':var newChar = 'R';break;
+case '3':var newChar = '4';break;
+case '4':var newChar = '&';break;
+case '5':var newChar = '!';break;
+case '6':var newChar = '%';break;
+case '7':var newChar = 't';break;
+
+case '8':var newChar = 'o';break;
+
+case '9':var newChar = '0';break;
+
+case '-':var newChar = '(';break;
+
+case '=':var newChar = '_';break;
+
+case '!':var newChar = 'c';break;
+
+case '@':var newChar = 'Z';break;
+
+case '#':var newChar = 'A';break;
+
+case '$':var newChar = 'Q';break;
+
+case '%':var newChar = '?';break;
+
+case '^':var newChar = 'l';break;
+
+case '&':var newChar = '`';break;
+
+case '*':var newChar = 'y';break;
+
+case '(':var newChar = '3';break;
+
+case ')':var newChar = 'S';break;
+
+case '_':var newChar = '5';break;
+
+case '+':var newChar = '-';break;
+
+case '`':var newChar = 'e';break;
+
+case '~':var newChar = '[';break;
+    }
+    return newChar
+}
+function changeBack(val){
+    var newChar = '';
+    switch (val) {
+        case '|':var newChar = ' ';break;
+        case '2':var newChar = 'q';break;
+case 'U':var newChar = 'w';break;
+case '$':var newChar = 'e';break;
+case '*':var newChar = 'r';break;
+case '~':var newChar = 't';break;
+case '8':var newChar = 'y';break;
+case ':':var newChar = 'u';break;
+case 'J':var newChar = 'i';break;
+case '.':var newChar = 'o';break;
+case 'C':var newChar = 'p';break;
+case "'":var newChar = '[';break;
+case 'v':var newChar = ']';break;
+case 'O':var newChar = 'q';break;
+case ']':var newChar = 'W';break;
+case 'p':var newChar = 'E';break;
+case 'n':var newChar = 'R';break;
+case 'a':var newChar = 'T';break;
+case 'P':var newChar = 'Y';break;
+case '1':var newChar = 'U';break;
+case 'B':var newChar = 'I';break;
+case 'z':var newChar = 'O';break;
+case 'r':var newChar = 'P';break;
+case 'W':var newChar = '{';break;
+case ',':var newChar = '}';break;
+case '@':var newChar = 'a';break;
+case 'N':var newChar = 's';break;
+case 'F':var newChar = 'd';break;
+case '0':var newChar = 'f';break;
+case '}':var newChar = 'g';break;
+case '=':var newChar = 'h';break;
+case '"':var newChar = 'j';break;
+case 'u':var newChar = 'k';break;
+case '^':var newChar = 'l';break;
+case '<':var newChar = ';';break;
+case 'X':var newChar = "'";break;
+case '/':var newChar = 'A';break;
+case 'T':var newChar = 'S';break;
+case 'm':var newChar = 'D';break;
+case 's':var newChar = 'F';break;
+case 'k':var newChar = 'G';break;
+case 'K':var newChar = 'H';break;
+case 'M':var newChar = 'J';break;
+case 'g':var newChar = 'K';break;
+case 'b':var newChar = 'L';break;
+case '#':var newChar = ':';break;
+case 'D':var newChar = '"';break;
+case 'Y':var newChar = 'z';break;
+case 'H':var newChar = 'x';break;
+case 'D':var newChar = '"';break;
+case 'Y':var newChar = 'z';break;
+case 'H':var newChar = 'x';break;
+case '>':var newChar = 'c';break;
+case 'q':var newChar = 'v';break;
+case 'w':var newChar = 'b';break;
+case 'E':var newChar = 'n';break;
+case '9':var newChar = 'm';break;
+case ';':var newChar = ',';break;
+case ')':var newChar = '.';break;
+case 'f':var newChar = '/';break;
+case '+':var newChar = 'Z';break;
+case '6':var newChar = 'X';break;
+case 'G':var newChar = 'C';break;
+case 'I':var newChar = 'V';break;
+case 'h':var newChar = 'B';break;
+case '7':var newChar = 'N';break;
+case 'x':var newChar = 'M';break;
+case 'V':var newChar = '<';break;
+case 'd':var newChar = '>';break;
+case 'i':var newChar = '?';break;
+case '{':var newChar = '1';break;
+case 'R':var newChar = '2';break;
+case '4':var newChar = '3';break;
+case '&':var newChar = '4';break;
+case '!':var newChar = '5';break;
+case '%':var newChar = '6';break;
+case 't':var newChar = '7';break;
+case 'o':var newChar = '8';break;
+case '0':var newChar = '9';break;
+case '(':var newChar = '-';break;
+case '_':var newChar = '=';break;
+case 'c':var newChar = '!';break;
+case 'Z':var newChar = '@';break;
+case 'A':var newChar = '#';break;
+case 'Q':var newChar = '$';break;
+case '?':var newChar = '%';break;
+case 'l':var newChar = '^';break;
+case '`':var newChar = '&';break;
+case 'y':var newChar = '*';break;
+case '3':var newChar = '(';break;
+case 'S':var newChar = ')';break;
+case '5':var newChar = '_';break;
+case '-':var newChar = '+';break;
+case 'e':var newChar = '`';break;
+case '[':var newChar = '~';break;
     }
     return newChar
 }
